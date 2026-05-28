287d15ab240186e747c5e305b1c42e7ce8ed5f83
KM_CM <197471590+KM-CM@users.noreply.github.com>
2026-05-28 13:10:19 +0300

    Add files via upload

--- KM_CM's Addon SoundTrack Koltsovo Combat Theme/lua/autorun/client/KM_CMs_Addon_SoundTrack_KoltsovoCombatTheme.lua
+++ KM_CM's Addon SoundTrack Koltsovo Combat Theme/lua/autorun/client/KM_CMs_Addon_SoundTrack_KoltsovoCombatTheme.lua
@@ -1,12 +1,12 @@
 include "autorun/client/Director.lua"
 
-Director_Music( "MUS_KolstovoCombatTheme", "Music/KolstovoCombatTheme.wav" )
+Director_Music( "MUS_KoltsovoCombatTheme", "Music/KoltsovoCombatTheme.wav" )
 
-DIRECTOR_ALLOCATE_COMBAT_THEME( "DIRECTOR_TRACK_KolstovoCombatTheme", {
+DIRECTOR_ALLOCATE_COMBAT_THEME( "DIRECTOR_TRACK_KoltsovoCombatTheme", {
 	CheckIntro = function( self ) return true end,
 	Intro = function( self ) Director_Music_Stop( self, "Main" ) return true end,
 	Execute = function( self )
 		if self.m_flVolume <= 0 then Director_Music_Stop( self, "Main" ) return end
-		if !self.tHandles.Main then Director_Music_Play( self, "Main", "MUS_KolstovoCombatTheme" ) end
+		if !self.tHandles.Main then Director_Music_Play( self, "Main", "MUS_KoltsovoCombatTheme" ) end
 	end
 } )
